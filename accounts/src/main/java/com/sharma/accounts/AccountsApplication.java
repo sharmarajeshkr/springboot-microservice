8bcb8d3ce4822a85ead067172b449781b30d6e71
rajesh <rajesh9616@gmail.com>
2024-02-05 17:19:59 +0530

    Chages for configration: Profiles

--- accounts/src/main/java/com/sharma/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/sharma/accounts/AccountsApplication.java
@@ -1,5 +1,6 @@
 package com.sharma.accounts;
 
+import com.sharma.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,6 +16,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaRepositories("com.sharma.accounts.repository")
 @EntityScan("com.sharma.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 // ------------------------ Documentation -----------------------//
 @OpenAPIDefinition(
 		info = @Info(

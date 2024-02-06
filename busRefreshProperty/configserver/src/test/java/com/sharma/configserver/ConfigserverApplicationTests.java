4e4c083d9f04d69ac3e25d36ec12094896045c78
rajesh <rajesh9616@gmail.com>
2024-02-07 00:03:04 +0530

    BusRefreah Config anf Docker Compose changes

---
+++ busRefreshProperty/configserver/src/test/java/com/sharma/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,10 @@
+package com.sharma.configserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ConfigserverApplicationTests {
+
+}

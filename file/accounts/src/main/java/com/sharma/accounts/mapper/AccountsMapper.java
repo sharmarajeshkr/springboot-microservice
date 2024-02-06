70a89947551d1583886ff9e93addca162c62da1b
rajesh <rajesh9616@gmail.com>
2024-02-06 13:53:38 +0530

    Config for File Configration

---
+++ file/accounts/src/main/java/com/sharma/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package com.sharma.accounts.mapper;
+
+import com.sharma.accounts.dto.AccountsDto;
+import com.sharma.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}

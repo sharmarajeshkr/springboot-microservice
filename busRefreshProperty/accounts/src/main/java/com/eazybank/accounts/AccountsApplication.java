bb225b5309d980a5e91999c5d4b23c8846a0f9ae
rajesh <rajesh9616@gmail.com>
2024-02-09 14:09:29 +0530

    Config server

---
+++ busRefreshProperty/accounts/src/main/java/com/eazybank/accounts/AccountsApplication.java
@@ -0,0 +1,46 @@
+package com.eazybank.accounts;
+
+import com.eazybank.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.sharma.accounts.controller") })
+@EnableJpaRepositories("com.sharma.accounts.repository")
+@EntityScan("com.sharma.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@sharma.com",
+						url = "https://www.sharma.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.sharma.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.sharma.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}

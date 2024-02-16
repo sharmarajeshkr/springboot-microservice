337edb12f30032371f3d416170595ee4bd87db39
rajesh <rajesh9616@gmail.com>
2024-02-16 16:58:26 +0530

    Api Gateway Server :

--- ms-api-gateway/accounts/src/main/java/com/eazybank/accounts/AccountsApplication.java
+++ ms-api-gateway/accounts/src/main/java/com/eazybank/accounts/AccountsApplication.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -19,6 +20,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaRepositories("com.eazybank.accounts.repository")
 @EntityScan("com.eazybank.accounts.model")
 */
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
